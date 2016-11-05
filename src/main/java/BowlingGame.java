7e81ebcb8f80ef191551d880906bdba8ff5daa63
Qingfei.You <qingfeiyou@gmail.com>
2016-11-05 21:54:33 +0100

    feat(calculation): compute total score when there are spares

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,7 +1,17 @@
-import java.util.Arrays;
-
 public class BowlingGame {
+
+    private static final int TOTAL_SHOTS = 20;
+    public static final int MAX_FRAME_SCORE = 10;
+
     public int calculateSum(int[] scores) {
-        return Arrays.stream(scores).reduce(0, (left, right) -> left+right);
+        int sum = 0;
+        for (int i = 0; i < TOTAL_SHOTS; i += 2) {
+            int frameSum = scores[i] + scores[i + 1];
+            if (frameSum == MAX_FRAME_SCORE) {
+                frameSum += scores[i+2];
+            }
+            sum += frameSum;
+        }
+        return sum;
     }
 }
--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -18,4 +18,31 @@ public class CalculationTest {
         assertThat(sum).isEqualTo(50);
     }
 
+    @Test
+    public void should_add_next_shot_score_to_a_spare_not_at_the_end() {
+
+        // Given
+        int[] scores = {9,1, 5,5, 7,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0};
+        BowlingGame bowlingGame = new BowlingGame();
+
+        // When
+        int sum = bowlingGame.calculateSum(scores);
+
+        // Then
+        assertThat(sum).isEqualTo(39);
+    }
+
+    @Test
+    public void should_add_next_shot_score_to_a_spare_at_the_end() {
+
+        // Given
+        int[] scores = {9,1, 5,5, 7,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 8,2, 5};
+        BowlingGame bowlingGame = new BowlingGame();
+
+        // When
+        int sum = bowlingGame.calculateSum(scores);
+
+        // Then
+        assertThat(sum).isEqualTo(54);
+    }
 }
